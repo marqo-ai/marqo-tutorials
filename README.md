f6c84bd2bc925c46eb00bf0901ebef2cd913ee96
Ellie Sleightholm <elliesleightholm@Ellies-MacBook-Pro.local>
2024-07-08 13:36:03 +0100

    adding tutorials

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# marqo-tutorials
+# Marqo Tutorials
+
+This repository contains code to [Marqo](https://github.com/marqo-ai/marqo) tutorials.
+
+## What's in This Repository?
+### `beginner-tutorials`
+
+This directory contains beginner-level tutorials using Marqo. It contains:
+* `simple_movie_demo.py`: getting started with Marqo by searching for movies.
+* `simple_multimodal_search.py`: explore multimodal search with Marqo.
+
+## Coming Soon...
+This repository will continue to be updated as new tutorials (written and video) come out. Sign up to our [newsletter](https://marqo.ai/newsletter) to be notified when new tutorials get released! For more examples with Marqo you can visit our [examples folder](https://github.com/marqo-ai/marqo/tree/mainline/examples) or our [Marqo documentation](https://docs.marqo.ai/). 
---
+++ beginner-tutorials/simple_movie_demo.py
@@ -0,0 +1,50 @@
+import marqo
+
+# Run this commented code in your terminal
+# docker pull marqoai/marqo:latest
+# docker rm -f marqo
+# docker run --name marqo -it -p 8882:8882 marqoai/marqo:latest
+
+# Create a Marqo client
+mq = marqo.Client(url="http://localhost:8882")
+
+# Delete the movie index if it already exists
+try:
+    mq.index("movies-index").delete()
+except:
+    pass
+
+# Create the movie index 
+mq.create_index("movies-index", model="hf/e5-base-v2")
+
+# Add documents (movie descriptions) to the index
+mq.index("movies-index").add_documents(
+    [
+        {
+            "Title": "Inception",
+            "Description": "A mind-bending thriller about dream invasion and manipulation.",
+        },
+        {
+            "Title": "Shrek",
+            "Description": "An ogre's peaceful life is disrupted by a horde of fairy tale characters who need his help.",
+        },
+        {
+            "Title": "Interstellar",
+            "Description": "A team of explorers travel through a wormhole in space to ensure humanity's survival.",
+        },
+        {
+            "Title": "The Martian",
+            "Description": "An astronaut becomes stranded on Mars and must find a way to survive.",
+        },
+    ],
+    tensor_fields=["Description"],
+)
+
+# Perform a search query on the index
+results = mq.index("movies-index").search(
+    q="Which movie is about space exploration?"
+)
+
+# Print the search results
+for result in results['hits']:
+    print(f"Title: {result['Title']}, Description: {result['Description']}. Score: {result['_score']}")
